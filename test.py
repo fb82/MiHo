256101fd3848b08dee313a9cbaf152785a52014e
lcmrl <lcmorelli.eng@gmail.com>
2024-03-20 23:05:40 +0100

    DIM as library

--- test.py
+++ test.py
@@ -12,6 +12,153 @@ import shutil
 from PIL import Image
 from miho_buffered_rot_patch_pytorch import *
 
+import sqlite3
+import yaml
+from importlib import import_module
+from pprint import pprint
+from deep_image_matching import logger, timer
+from deep_image_matching.config import Config
+from deep_image_matching.image_matching import ImageMatching
+from deep_image_matching.io.h5_to_db import export_to_colmap
+
+
+def resize_image(img, max_side_length):
+    height, width = img.shape[:2]
+    if max(height, width) > max_side_length:
+        scale_factor = max_side_length / max(height, width)
+        img_resized = cv2.resize(
+            img, (int(width * scale_factor), int(height * scale_factor))
+        )
+        return img_resized
+    else:
+        return img
+
+
+def GeneratePlot(
+    img0_path: Path,
+    img1_path: Path,
+    kpts0: np.ndarray,
+    kpts1: np.ndarray,
+    matches: np.ndarray,
+):
+    # Load images
+    img0 = cv2.imread(str(img0_path))
+    img1 = cv2.imread(str(img1_path))
+    # Convert keypoints to integers
+    kpts0_int = np.round(kpts0).astype(int)
+    kpts1_int = np.round(kpts1).astype(int)
+    # Create a new image to draw matches
+    img_matches = np.zeros(
+        (max(img0.shape[0], img1.shape[0]), img0.shape[1] + img1.shape[1], 3),
+        dtype=np.uint8,
+    )
+    img_matches[: img0.shape[0], : img0.shape[1]] = img0
+    img_matches[: img1.shape[0], img0.shape[1] :] = img1
+    # Show keypoints
+    for kpt in kpts0_int:
+        kpt = tuple(kpt)
+        cv2.circle(img_matches, kpt, 3, (0, 0, 255), -1)
+    for kpt in kpts1_int:
+        kpt = tuple(kpt + np.array([img0.shape[1], 0]))
+        cv2.circle(img_matches, kpt, 3, (0, 0, 255), -1)
+    # Draw lines and circles for matches
+    for match in matches:
+        pt1 = tuple(kpts0_int[match[0]])
+        pt2 = tuple(np.array(kpts1_int[match[1]]) + np.array([img0.shape[1], 0]))
+        # Draw a line connecting the keypoints
+        cv2.line(img_matches, pt1, pt2, (0, 255, 0), 1)
+        # Draw circles around keypoints
+        cv2.circle(img_matches, pt1, 3, (255, 0, 0), -1)
+        cv2.circle(img_matches, pt2, 3, (255, 0, 0), -1)
+    img_matches_resized = resize_image(img_matches, 1000)
+    # Show the image with matches
+    cv2.imshow(f"Verified matches   {img0_path.name} - {img1_path.name}", img_matches_resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def pair_id_to_image_ids(pair_id):
+    image_id2 = pair_id % 2147483647
+    image_id1 = (pair_id - image_id2) / 2147483647
+    return image_id1, image_id2
+
+
+def dbReturnMatches(database_path, min_num_matches):
+    if os.path.exists(database_path):
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        images = {}
+        raw_matches = {}
+        matches = {}
+
+        cursor.execute("SELECT image_id, camera_id, name FROM images;")
+        for row in cursor:
+            image_id = row[0]
+            image_name = row[2]
+            images[image_id] = image_name
+
+        # Raw matches
+        cursor.execute(
+            "SELECT pair_id, data FROM matches WHERE rows>=?;",
+            (min_num_matches,),
+        )
+
+        for row in cursor:
+            pair_id = row[0]
+            _matches = np.fromstring(row[1], dtype=np.uint32).reshape(-1, 2)
+            image_id1, image_id2 = pair_id_to_image_ids(pair_id)
+            image_name1 = images[image_id1]
+            image_name2 = images[image_id2]
+            raw_matches["{} {}".format(image_name1, image_name2)] = _matches
+
+        # Geometric verified matches
+        cursor.execute(
+            "SELECT pair_id, data FROM two_view_geometries WHERE rows>=?;",
+            (min_num_matches,),
+        )
+
+        for row in cursor:
+            pair_id = row[0]
+            inlier_matches = np.fromstring(row[1], dtype=np.uint32).reshape(-1, 2)
+            image_id1, image_id2 = pair_id_to_image_ids(pair_id)
+            image_name1 = images[image_id1]
+            image_name2 = images[image_id2]
+            matches["{} {}".format(image_name1, image_name2)] = inlier_matches
+
+        cursor.close()
+        connection.close()
+
+        return images, raw_matches, matches
+
+    else:
+        print("Database does not exist")
+        quit()
+
+
+def dbReturnKeypoints(database_path):    
+    if os.path.exists(database_path):
+        connection = sqlite3.connect(database_path)
+        cursor = connection.cursor()
+
+        images = {}
+        keypoints = {}
+        cursor.execute("SELECT image_id, camera_id, name FROM images;")
+        for row in cursor:
+            image_id = row[0]
+            image_name = row[2]
+            images[image_id] = image_name
+
+        cursor.execute("SELECT image_id, rows, cols, data FROM keypoints")
+        for row in cursor:
+            image_id = row[0]
+            # kpts = np.fromstring(row[3], np.float32).reshape(-1, 6)
+            # kpts = np.fromstring(row[3], np.float32).reshape(-1, 4)
+            kpts = np.fromstring(row[3], np.float32).reshape(-1, 2)
+            keypoints[images[image_id]] = kpts
+
+    return images, keypoints
+
 def relative_pose_error(R_gt, t_gt, R, t, ignore_gt_t_thr=0.0):
     # angle error between 2 vectors
     # t_gt = T_0to1[:3, 3]
@@ -105,7 +252,7 @@ if __name__ == '__main__':
         pairs_per_scene[scene1].append((image1, image2, pair_idx))
     
 
-
+    # Setup DIM
     for sac_th_idx in range(len(sac_th)):
         th_str = 'th_'+str(sac_th[sac_th_idx])
         #print(matcher+' '+str(pixel_thr)+' '+th_str)
@@ -122,37 +269,92 @@ if __name__ == '__main__':
 
             shutil.copytree(working_dir / scene / 'color', working_dir / scene / 'images', dirs_exist_ok=True)
 
-            # STILL TO BE DISABLED RANSAC
-            # Extract and match features
-            p = subprocess.Popen([
-                "python", 
-                "./thirdparty/deep-image-matching/main.py", 
-                "--dir", f"{working_dir / scene}", 
-                "--pipeline", 
-                f"{pipeline}", 
-                "--strategy", 
-                "custom_pairs", 
-                "--force", 
-                "--skip_reconstruction",
-                "--pair_file",
-                f"{working_dir / scene}/pairs.txt",
-                ])
-            p.wait()
+            # Run DIM as library to extract and match features
+            cli_params = {
+                "dir": f"{working_dir / scene}",
+                "pipeline": f"{pipeline}",
+                "strategy": "custom_pairs",
+                "pair_file": f"{working_dir / scene}/pairs.txt",
+                "tiling": "none",
+                "skip_reconstruction": True,
+                "force": True,
+                "camera_options": "../config/cameras.yaml",
+                "openmvg": None,
+            }
 
-            # Save matches in x1 y1 x2 y2 format
-            p = subprocess.Popen([
-                "python", 
-                "./thirdparty/deep-image-matching/scripts/export_from_database.py", 
-                f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/database.db", 
-                "x1y1x2y2",
-                ])
-            p.wait()
+            config = Config(cli_params)
+            config.general["min_inliers_per_pair"] = 10
+            config.general["gv_threshold"] = 1000 # 1000 pixel error threshold to disable DIM ransac
+            config.extractor["max_keypoints"] = 1000
+            config.matcher["filter_threshold"] = 0.1
+            config.save()
+
+            imgs_dir = config.general["image_dir"]
+            output_dir = config.general["output_dir"]
+            matching_strategy = config.general["matching_strategy"]
+            extractor = config.extractor["name"]
+            matcher = config.matcher["name"]
+
+            img_matching = ImageMatching(
+                imgs_dir=imgs_dir,
+                output_dir=output_dir,
+                matching_strategy=matching_strategy,
+                local_features=extractor,
+                matching_method=matcher,
+                pair_file=config.general["pair_file"],
+                retrieval_option=config.general["retrieval"],
+                overlap=config.general["overlap"],
+                existing_colmap_model=config.general["db_path"],
+                custom_config=config.as_dict(),
+            )
+
+
+            pair_path = img_matching.generate_pairs()
+            feature_path = img_matching.extract_features()
+            match_path = img_matching.match_pairs(feature_path)
+
+            camera_options = {
+               'general' : {
+                "camera_model" : "pinhole", # ["simple-pinhole", "pinhole", "simple-radial", "opencv"]
+                "single_camera" : True,
+               },
+               'cam0' : {
+                    "camera_model" : "pinhole",
+                    "images" : "DSC_6468.JPG,DSC_6468.JPG",
+               },
+               'cam1' : {
+                    "camera_model" : "pinhole",
+                    "images" : "",
+               },
+            }
+
+            database_path = output_dir / "database.db"
+            export_to_colmap(
+                img_dir=imgs_dir,
+                feature_path=feature_path,
+                match_path=match_path,
+                database_path=database_path,
+                camera_options=camera_options,
+            )
+
+            images, keypoints = dbReturnKeypoints(database_path)
+            images, raw_matches, matches = dbReturnMatches(database_path, min_num_matches=1)
+
+            ## Save matches in x1 y1 x2 y2 format
+            #p = subprocess.Popen([
+            #    "python", 
+            #    "./thirdparty/deep-image-matching/scripts/export_from_database.py", 
+            #    f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/database.db", 
+            #    "x1y1x2y2",
+            #    ])
+            #p.wait()
 
             for pair in pairs_per_scene[scene]:
                 img0, img1, pair_idx = pair[0], pair[1], pair[2]
-                match_file1 = f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/matches/{Path(img0).stem}_{Path(img1).stem}.txt"
-                match_file2 = f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/matches/{Path(img1).stem}_{Path(img0).stem}.txt"
-
+                #match_file1 = f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/matches/{Path(img0).stem}_{Path(img1).stem}.txt"
+                #match_file2 = f"{working_dir / scene}/results_{pipeline}_custom_pairs_quality_high/matches/{Path(img1).stem}_{Path(img0).stem}.txt"
+                pair01 = f"{Path(img0).name} {Path(img1).name}"
+                pair10 = f"{Path(img1).name} {Path(img0).name}"
                 #p = subprocess.Popen([
                 #    "python",
                 #    "./miho_buffered_rot_patch_pytorch.py",
@@ -165,18 +367,34 @@ if __name__ == '__main__':
                 im1 = Image.open(working_dir / scene / "images" / img0)            
                 im2 = Image.open(working_dir / scene / "images" / img1)
                 
-                if os.path.exists(match_file1):
-                    m12 = np.loadtxt(match_file1)
-                if os.path.exists(match_file2):
-                    m21 = np.loadtxt(match_file2)
-                    m12 = m21[:, [2, 3, 0, 1]]
+                #if os.path.exists(match_file1):
+                #    m12 = np.loadtxt(match_file1)
+                #if os.path.exists(match_file2):
+                #    m21 = np.loadtxt(match_file2)
+                #    m12 = m21[:, [2, 3, 0, 1]]
+
+                m01 = None
                 
-                if os.path.exists(match_file1) or os.path.exists(match_file2):
+                if pair01 in list(matches.keys()):
+                    m01 = matches[pair01]
+                    k0 = keypoints[f"{Path(img0).name}"][m01[:,0],:]
+                    k1 = keypoints[f"{Path(img1).name}"][m01[:,1],:]
+                    #GeneratePlot(working_dir / scene / "images" / Path(img0), working_dir / scene / "images" / Path(img1), keypoints[f"{Path(img0).name}"], keypoints[f"{Path(img1).name}"], m01)
+                    m01 = np.hstack((k0,k1))
+
+                if pair10 in list(matches.keys()):
+                    m10 = matches[pair10]
+                    k1 = keypoints[f"{Path(img1).name}"][m10[:,0],:]
+                    k0 = keypoints[f"{Path(img0).name}"][m10[:,1],:]
+                    m01 = np.hstack((k0,k1))
+                
+                #if os.path.exists(match_file1) or os.path.exists(match_file2):
+                if m01.any != None:
 
                     params = miho.all_params()
                     params['get_avg_hom']['rot_check'] = True
                     mihoo = miho(params)
-                    mihoo.planar_clustering(m12[:, :2], m12[:, 2:])
+                    mihoo.planar_clustering(m01[:, :2], m01[:, 2:])
                     # Ransac
                     # Comprimere i file txt!
                     mihoo.attach_images(im1, im2)
