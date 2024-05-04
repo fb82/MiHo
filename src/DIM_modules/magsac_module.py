ce899f22cc111a083d1b1e6df560cad1c665c969
lcmrl <lcmorelli.eng@gmail.com>
2024-05-04 16:40:52 +0200

    added magsac

---
+++ src/DIM_modules/magsac_module.py
@@ -0,0 +1,55 @@
+import torch
+import numpy as np
+import pydegensac
+from deep_image_matching.utils.geometric_verification import geometric_verification
+
+class magsac_module:
+    def __init__(self, **args):
+        self.px_th = 3
+        self.conf = 0.9999
+        self.max_iters = 100000
+              
+        for k, v in args.items():
+           setattr(self, k, v)
+       
+        
+    def get_id(self):
+        return ('magsac_th_' + str(self.px_th) + '_conf_' + str(self.conf) + '_max_iters_' + str(self.max_iters)).lower()
+
+    
+    def eval_args(self):
+        return "pipe_module.run(pt1, pt2, Hs)"
+
+        
+    def eval_out(self):
+        return "pt1, pt2, Hs, F, mask = out_data"
+    
+    
+    def run(self, *args):
+        pt1 = args[0]
+        pt2 = args[1]
+        Hs = args[2]
+        
+        if torch.is_tensor(pt1):
+            pt1 = pt1.detach().cpu()
+            pt2 = pt1.detach().cpu()
+            
+        if (np.ascontiguousarray(pt1).shape)[0] > 7:                      
+            F, mask = geometric_verification(
+                kpts0=np.ascontiguousarray(pt1),
+                kpts1=np.ascontiguousarray(pt2),
+                method="MAGSAC",
+                threshold=self.px_th,
+                confidence=self.conf,
+                max_iters=self.max_iters,
+                quiet=False,
+            )
+    
+            pt1 = args[0][mask]
+            pt2 = args[1][mask]     
+            Hs = args[2][mask]
+        else:            
+            F = None
+            mask = None
+            
+        return pt1, pt2, Hs, F, mask
