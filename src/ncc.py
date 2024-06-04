7021fce43351c377e68b49f980e9c91df71b8428
fb82 <bellavia.fabio@gmail.com>
2024-06-04 10:35:41 +0200

    params improvements

--- src/ncc.py
+++ src/ncc.py
@@ -501,7 +501,7 @@ class ncc_module:
 
         laf_is_better = np.NaN
         if self.also_prev and ('Hs_prev' in args.keys()) and (args['Hs'].size()[0] > 0):
-            pt1_, pt2_, Hs_ncc_, val_, T_ = refinement_norm_corr_alternate(im1, im2, args['pt1'], args['pt2'], args['Hs_prev'], w=self.w, w_big=self.w_big, ref_image=[self.ref_images], angle=self.angle, scale=self.scale, subpix=self.subpix, img_patches=False)   
+            pt1_, pt2_, Hs_ncc_, val_, T_ = refinement_norm_corr_alternate(im1, im2, args['pt1'], args['pt2'], args['Hs_prev'], w=self.w, w_big=self.w_big, ref_image=[self.ref_images], angle=[0, ], scale=[[1, 1], ], subpix=self.subpix, img_patches=False)   
             replace_idx = torch.argwhere((torch.cat((val.unsqueeze(0),val_.unsqueeze(0)), dim=0)).max(dim=0)[1] == 1)
             pt1[replace_idx] = pt1_[replace_idx]
             pt2[replace_idx] = pt2_[replace_idx]
