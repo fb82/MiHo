17081300f2cf11f6546267f1834039c7669d263f
fb82 <bellavia.fabio@gmail.com>
2024-11-01 11:08:27 +0100

    Update run_bench_time.py

--- run_bench_time.py
+++ run_bench_time.py
@@ -224,8 +224,8 @@ if __name__ == '__main__':
                         if hasattr(pipe_module, 'mode'): setattr(pipe_module, 'mode', bench_mode)
                         if hasattr(pipe_module, 'outdoor'): setattr(pipe_module, 'outdoor', benchmark_data[b]['is_outdoor'])
 
-                    # bench.run_pipe(pipe, b_data, benchmark_data[b]['name'], benchmark_data[b]['Name'], bench_path=bench_path, ext=benchmark_data[b]['ext'], running_time=True)
-                    # bench.count_pipe_match(pipe, b_data, benchmark_data[b]['name'], bench_path=bench_path, save_to=to_save_file + 'match_count' + to_save_file_suffix + '.pbz2')
+                    bench.run_pipe(pipe, b_data, benchmark_data[b]['name'], benchmark_data[b]['Name'], bench_path=bench_path, ext=benchmark_data[b]['ext'], running_time=True)
+                    bench.count_pipe_match(pipe, b_data, benchmark_data[b]['name'], bench_path=bench_path, save_to=to_save_file + 'match_count' + to_save_file_suffix + '.pbz2')
                     bench.collect_pipe_time(pipe, b_data, benchmark_data[b]['name'], bench_path=bench_path, save_to=to_save_file + 'runtime' + to_save_file_suffix + '.pbz2')
 
                     if benchmark_data[b]['is_not_planar']:
